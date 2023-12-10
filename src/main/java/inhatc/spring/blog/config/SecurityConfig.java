b4850c3cc9b2240b08af6fe4a7cc7066b76e6564
godee95 <50854903+godee95@users.noreply.github.com>
2023-12-10 22:58:31 +0900

    feat : jwt provider

--- src/main/java/inhatc/spring/blog/config/SecurityConfig.java
+++ src/main/java/inhatc/spring/blog/config/SecurityConfig.java
@@ -2,28 +2,59 @@ package inhatc.spring.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import inhatc.spring.blog.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter wAuthenticationFilter;
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-            .formLogin(Customizer.withDefaults())
-            .logout(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+            // .formLogin(Customizer.withDefaults())
+            // .formLogin(login -> login
+            //                     .loginPage("/member/login")
+            //                     .permitAll()
+            //                     )   
+            // .logout(Customizer.withDefaults());
+
+        http
+            .authorizeHttpRequests(request -> request
+                                    .requestMatchers("/**").permitAll() // 모든 경로 인증 하지 않음.
+                                    .requestMatchers(HttpMethod.POST, "/member/new").permitAll()
+                                    // .requestMatchers(HttpMethod.GET, "/board/**").permitAll()
+                                    .anyRequest().authenticated());
+
+        // 각 페이지에 대한 접근 권한 설정
+        // http.authorizeHttpRequests(request -> request
+        //         .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+        //         .requestMatchers("/", "/member/**", "/user/**", "/login/**").permitAll()
+        //         .anyRequest().authenticated());
+
+        // http.exceptionHandling(exception -> exception
+        //         .authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+
+        http.addFilterBefore(wAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // http.formLogin(form -> form
+        //                 .usernameParameter("email")
+        //                 .passwordParameter("password")
+        //                 .permitAll()
+        //                 );
+
+        // http.logout(Customizer.withDefaults());
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/inhatc/spring/blog/service/UserService.java
+++ src/main/java/inhatc/spring/blog/service/UserService.java
@@ -3,6 +3,7 @@ package inhatc.spring.blog.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public User saveUser(UserDto userDto){
         String encryptPassword = passwordEncoder.encode(userDto.getPassword());
